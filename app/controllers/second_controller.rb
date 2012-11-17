8ea22b9a4392716a42b1fac43eb8bd956b76867a
Daniel Krusenstråhle <dkrusenstrahle@gmail.com>
2012-11-17 17:48:10 +0100

    refactored

--- app/controllers/second_controller.rb
+++ app/controllers/second_controller.rb
@@ -1,7 +1,7 @@
 class SecondController < UIViewController
   def initWithNibName(name, bundle: bundle)
     super
-    customize
+    add_elements
     self
   end
 
@@ -9,12 +9,14 @@ class SecondController < UIViewController
     super
   end
 
-  def customize
+  def add_elements
 
-    # Set the main view attributes
+    # Set views title in the navigationBar
 
     self.title = "Second"
-    
+
+    # Set the window background color
+
     view.backgroundColor = UIColor.whiteColor 
 
     # Create the images
@@ -22,20 +24,20 @@ class SecondController < UIViewController
     tabNormal = UIImage.imageNamed('tabbar-activity-selected.png')
     tabSelected = UIImage.imageNamed('tabbar-activity-selected.png')
 
-    backNormal = UIImage.imageNamed('nav-backbutton.png').stretchableImageWithLeftCapWidth(14, topCapHeight:0)
-    backPressed = UIImage.imageNamed('nav-backbutton.png').stretchableImageWithLeftCapWidth(14, topCapHeight:0)
+    backButtonNormal = UIImage.imageNamed('nav-backbutton.png').stretchableImageWithLeftCapWidth(14, topCapHeight:0)
+    backButtonSelected = UIImage.imageNamed('nav-backbutton.png').stretchableImageWithLeftCapWidth(14, topCapHeight:0)
 
     # Style the tabBar item
 
     self.tabBarItem = UITabBarItem.alloc.initWithTitle('Second', image: nil, tag: 0)
     self.tabBarItem.setFinishedSelectedImage(tabSelected, withFinishedUnselectedImage: tabNormal) 
 
-    # Style the back button
+    # Style and adjust the back button
 
     offset = UIOffset.new(3, 0)
 
-    UIBarButtonItem.appearance.setBackButtonBackgroundImage(backNormal, forState:UIControlStateNormal, barMetrics:UIBarMetricsDefault)
-    UIBarButtonItem.appearance.setBackButtonBackgroundImage(backPressed, forState: UIControlStateHighlighted, barMetrics: UIBarMetricsDefault)
+    UIBarButtonItem.appearance.setBackButtonBackgroundImage(backButtonNormal, forState:UIControlStateNormal, barMetrics:UIBarMetricsDefault)
+    UIBarButtonItem.appearance.setBackButtonBackgroundImage(backButtonSelected, forState: UIControlStateHighlighted, barMetrics: UIBarMetricsDefault)
     UIBarButtonItem.appearance.setBackButtonTitlePositionAdjustment(offset, forBarMetrics:UIBarMetricsDefault)
   end
 end
